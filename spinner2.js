7038637412809f7e6a4c81efcd4c4aaf422ad3a3
Sneha Mahajan <sneh.km@gmail.com>
2022-04-14 16:18:32 -0400

    Removed comments from spinner2.js

--- spinner2.js
+++ spinner2.js
@@ -1,39 +1,3 @@
-// setTimeout(() => {
-//   process.stdout.write("\r|   ");
-// }, 100);
-
-// setTimeout(() => {
-//   process.stdout.write("\r/   ");
-// }, 300);
-
-// setTimeout(() => {
-//   process.stdout.write("\r-   ");
-// }, 500);
-
-// setTimeout(() => {
-//   process.stdout.write("\r\\   ");
-// }, 700);
-
-// setTimeout(() => {
-//   process.stdout.write("\r|   ");
-// }, 900);
-
-// setTimeout(() => {
-//   process.stdout.write("\r/   ");
-// }, 1100);
-
-// setTimeout(() => {
-//   process.stdout.write("\r-   ");
-// }, 1300);
-
-// setTimeout(() => {
-//   process.stdout.write("\r\\   ");
-// }, 1500);
-
-// setTimeout(() => {
-//   process.stdout.write("\r|   ");
-// }, 1700);
-
 let delay = 200;
 const printNewLine = () => process.stdout.write("\n");
 const spinnerChars = ["|", "/", "-", "\\", "|", "/", "-", "\\", "|"];
